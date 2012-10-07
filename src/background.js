6c05f0f52726100aa8b9789a3a49803f4434973e
Gil Megidish <gil@megidish.net>
2012-10-07 12:54:37 +0200

    better and faster code

--- src/background.js
+++ src/background.js
@@ -1,8 +1,7 @@
-function run()
-{
-	var imgs = document.querySelectorAll("img");
-	for (var i=0; i<imgs.length; i++)
-	{
+function run() {
+	// select only images that haven't been processed before
+	var imgs = document.querySelectorAll("img:not([x-crispy2d=true])");
+	for (var i=0; i<imgs.length; i++) {
 		var img = imgs[i];
 
 		if (img.complete == false) {
@@ -10,55 +9,65 @@ function run()
 			continue;
 		}
 
-		if (img.parentNode != null && img.parentNode.getAttribute("ext-canvas2d") == "true") {
-			// already patched
+		// mark this image as processed, regardless if a canvas was
+		// wrapped around it.
+		img.setAttribute("x-crispy2d", "true");
+
+		// check if is a .png, .bmp, or .gif
+		var src = img.src.toLowerCase();
+		var ext = src.substr(-4);
+		if (ext != ".png" && ext != ".gif" && ext != ".bmp") {
+			// ignore this file
 			continue;
 		}
 
-		var kx = img.width/img.naturalWidth;
-		var ky = img.height/img.naturalHeight;
+		var width = img.width;
+		var height = img.height;
 
-		if (kx == ky && Math.floor(kx) == kx && kx >= 2)
-		{
-			// check if is a .png, .bmp, or .gif
-			var src = img.src.toLowerCase();
-			var ext = src.substr(-4);
-			if (ext != ".png" && ext != ".gif" && ext != ".bmp")
-			{
-				// ignore this file
-				continue;
-			}
+		// use computed style if possible, this is because some sites,
+		// (lemonamiga for example) use 100% as image size, rather than
+		// provide pixel values.
+		var css = document.defaultView.getComputedStyle(img, null);
+		if (css != null) {
+			width = parseInt(css['width']);
+			height = parseInt(css['height']);
+		}
 
+		var kx = width/img.naturalWidth;
+		var ky = height/img.naturalHeight;
+
+		// only operate on 2x and onwards
+		if (kx == ky && Math.floor(kx) == kx && kx >= 2) {
 			// create canvas as big as scaled output
 			var canvas = document.createElement("canvas");
-			canvas.width = img.width;
-			canvas.height = img.height;
+			canvas.width = width;
+			canvas.height = height;
 
 			// first draw the 1:1 image, so we can get its pixels
 			var ctx = canvas.getContext("2d");
 			ctx.drawImage(img, 0, 0);
 				
 			var datain = ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight);
-			var dataout = ctx.createImageData(img.width, img.height);
+			var dataout = ctx.createImageData(width, height);
 
 			var ptrin = datain.data;
 			var ptrout = dataout.data;
 
-			for (var y=0; y<datain.height; y++)
-			{
-				// each row, scale horizontally
-				var offsetin = y*datain.width*4;
-				var offsetout = y*dataout.width*ky*4;
+			var stridein = datain.width*4;
+			var strideout = dataout.width*4;
 
-				for (var x=0; x<datain.width; x++)
-				{
+			for (var y=0; y<datain.height; y++) {
+				// each row, scale horizontally
+				var offsetin = y*stridein;
+				var offsetout = y*ky*strideout;
+
+				for (var x=0; x<datain.width; x++) {
 					var r = ptrin[offsetin++];
 					var g = ptrin[offsetin++];
 					var b = ptrin[offsetin++];
 					var a = ptrin[offsetin++];
 
-					for (var x0=0; x0<kx; x0++)
-					{
+					for (var x0=0; x0<kx; x0++) {
 						ptrout[offsetout++] = r;
 						ptrout[offsetout++] = g;
 						ptrout[offsetout++] = b;
@@ -66,16 +75,16 @@ function run()
 					}
 				}
 
-				var row = dataout.data.subarray(y*ky*dataout.width*4, (y*ky+1)*dataout.width*4);
-				for (y0=1; y0<ky; y0++)
-				{
-					dataout.data.set(row, (y*ky+y0)*dataout.width*4);
+				var row = dataout.data.subarray(y*ky*strideout, (y*ky+1)*strideout);
+				for (y0=1; y0<ky; y0++) {
+					dataout.data.set(row, (y*ky+y0)*strideout);
 				}
 			}
 
 			// paste result back onto cavnas
 			ctx.putImageData(dataout, 0, 0);
 
+			// wrap it with a div
 			var div = document.createElement("div");
 			div.style.display = "inline";
 			div.style.width = canvas.width + "px";
@@ -91,6 +100,8 @@ function run()
 
 			div.style.position = "relative";
 
+			// keep the image there, transparent, so the user
+			// can still do right-click and save 
 			img.style.position = "absolute";
 			img.style.top = -img.height;
 			img.style.left = "0px";
@@ -100,9 +111,6 @@ function run()
 			// add these two children to img
 			div.appendChild(canvas);
 			div.appendChild(img);
-
-			// so we don't loop
-			div.setAttribute("ext-canvas2d", "true");
 		}
 	}
 }
